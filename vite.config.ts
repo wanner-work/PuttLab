85e518071f5545df1dbfc8eb4c33c47c539b857f
jwanner83 <jonas@wanner.com>
2025-09-12 10:45:31 +0200

    adding shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()]
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
