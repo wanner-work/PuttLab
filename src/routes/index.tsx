d6b48702c73eb406ce4df20ab17bc56e1feae01f
jwanner83 <jonas@wanner.com>
2025-09-16 13:55:46 +0200

    adding logo component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import AnalyticsCard from '@/components/analytics/AnalyticsCard'
 import PageContainer from '@/components/basic/PageContainer'
+import Logo from '@/components/brand/Logo'
 import { Button } from '@/components/ui/button'
 import { createFileRoute, Link } from '@tanstack/react-router'
 
@@ -9,11 +10,12 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <PageContainer
-      className="pt-12 pb-26"
-      title="PuttLab"
-      subtitle="Welcome to PuttLab"
-    >
+    <PageContainer className="pb-26">
+      <Logo className="max-w-full px-12" />
+      <p className="text-muted-foreground mx-auto -mt-2 mb-8 max-w-64 text-center text-balance">
+        Track and analyze your putting practice.
+      </p>
+
       <AnalyticsCard />
       <div className="fixed bottom-6 left-1/2 w-full -translate-x-1/2 px-6">
         <Link to="/sessions" className="w-full">
