e711f95f686c8399fa0ca8e390b0d7acbb4b8c30
jwanner83 <jonas@wanner.com>
2025-09-26 07:31:39 +0200

    remove aggressive border arround wheel

--- src/components/ui/wheel-picker.tsx
+++ src/components/ui/wheel-picker.tsx
@@ -14,7 +14,7 @@ function WheelPickerWrapper({
   return (
     <WheelPickerPrimitive.WheelPickerWrapper
       className={cn(
-        'w-56 rounded-lg border border-zinc-200 bg-white px-1 shadow-xs dark:border-zinc-700/80 dark:bg-black',
+        'w-56 rounded-lg border bg-white px-1 shadow-xs dark:bg-black',
         '*:data-rwp:first:*:data-rwp-highlight-wrapper:rounded-s-md',
         '*:data-rwp:last:*:data-rwp-highlight-wrapper:rounded-e-md',
         className
