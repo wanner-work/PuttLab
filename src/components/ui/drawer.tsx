c90f3a0bb56e96ba95b20dffbceba000cd822494
jwanner83 <jonas@wanner.com>
2025-10-21 15:23:49 +0200

    adding scale background on vaul

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -6,7 +6,9 @@ import { cn } from '@/lib/utils'
 function Drawer({
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Root>) {
-  return <DrawerPrimitive.Root data-slot="drawer" {...props} />
+  return (
+    <DrawerPrimitive.Root shouldScaleBackground data-slot="drawer" {...props} />
+  )
 }
 
 function DrawerTrigger({
