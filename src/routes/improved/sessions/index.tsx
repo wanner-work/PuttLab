d4b8fabbba5b12adc52b9f049b8cf97630b70827
jwanner83 <jonas@wanner.com>
2025-10-23 14:32:31 +0200

    adding create session to sessions route

--- src/routes/improved/sessions/index.tsx
+++ src/routes/improved/sessions/index.tsx
@@ -4,11 +4,15 @@ import Layout from '@/components/common/layout/Layout'
 import AnimateLoading from '@/components/common/loading/AnimateLoading'
 import SessionList from '@/components/pages/sessions/list/SessionList'
 import SessionListFilter from '@/components/pages/sessions/list/SessionListFilter'
+import CreateSessionDrawer from '@/components/sessions/actions/CreateSessionDrawer'
+import { Button } from '@/components/ui/button'
+import type { Session } from '@/data/entities/session'
 import useSessions from '@/hooks/data/sessions/useSessions'
 import useSessionsFilter from '@/hooks/data/sessions/useSessionsFilter'
 import { createFileRoute } from '@tanstack/react-router'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import { PlusIcon } from 'lucide-react'
 import { memo, useState } from 'react'
 
 dayjs.extend(relativeTime)
@@ -18,14 +22,38 @@ export const Route = createFileRoute('/improved/sessions/')({
 })
 
 function Sessions() {
+  const navigate = Route.useNavigate()
+
   const [filter, setFilter] = useState<number | null>(null)
+  const [createDrawerOpen, setCreateDrawerOpen] = useState(false)
+
   const { isLoading, sessions } = useSessions()
   const filteredSessions = useSessionsFilter(sessions || [], filter)
+
+  const onCreateSuccess = (session: Session | undefined) => {
+    setCreateDrawerOpen(false)
+
+    if (session) {
+      navigate({
+        to: '/improved/sessions/$sessionId',
+        params: {
+          sessionId: String(session.id)
+        },
+        viewTransition: { types: ['slide-left'] }
+      })
+    }
+  }
 
   return (
     <Layout rows={['auto', 'auto', 'auto', '1fr']} className="pb-0">
       <Header backTo="/improved" />
       <Headline title="Sessions" subtitle="Manage your sessions effectively" />
+
+      <CreateSessionDrawer
+        open={createDrawerOpen}
+        onOpenChange={setCreateDrawerOpen}
+        onSuccess={onCreateSuccess}
+      />
 
       <SessionListFilter sessions={sessions || []} onFilter={setFilter} />
 
@@ -39,6 +67,16 @@ function Sessions() {
           />
         )}
       />
+
+      <div className="fixed bottom-6 left-0 flex w-full justify-center">
+        <Button
+          onClick={() => setCreateDrawerOpen(true)}
+          className="rounded-full"
+        >
+          <PlusIcon />
+          Create Session
+        </Button>
+      </div>
     </Layout>
   )
 }
