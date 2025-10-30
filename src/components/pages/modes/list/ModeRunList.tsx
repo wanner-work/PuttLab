22480c28aa37aaadd550e1149eca580fb95d7d46
jwanner83 <jonas@wanner.com>
2025-10-30 08:53:06 +0100

    adding mode run deletion

--- src/components/pages/modes/list/ModeRunList.tsx
+++ src/components/pages/modes/list/ModeRunList.tsx
@@ -1,6 +1,8 @@
+import DeleteModeRunDrawer from '@/components/sessions/actions/DeleteModeRunDrawer'
 import type { ModeRun } from '@/data/entities/moderun'
 import type ModeDefinition from '@/interfaces/data/mode/ModeDefinition'
 import { motion } from 'motion/react'
+import { useState } from 'react'
 import { List } from 'react-window'
 import ModeRunListItem from './ModeRunListItem'
 
@@ -11,6 +13,14 @@ interface Props {
 }
 
 export default function ModeRunList({ modeRuns, mode, wasLoaded }: Props) {
+  const [modeRunToDelete, setModeRunToDelete] = useState<ModeRun | null>(null)
+
+  const handleOpenChange = (open: boolean) => {
+    if (!open) {
+      setModeRunToDelete(null)
+    }
+  }
+
   return (
     <motion.div
       initial={{
@@ -19,11 +29,18 @@ export default function ModeRunList({ modeRuns, mode, wasLoaded }: Props) {
       }}
       animate={{ opacity: 1, clipPath: 'inset(0% 0% 0% 0%)' }}
     >
+      <DeleteModeRunDrawer
+        open={!!modeRunToDelete}
+        onOpenChange={handleOpenChange}
+        onSuccess={() => setModeRunToDelete(null)}
+        modeId={mode.id}
+        modeRun={modeRunToDelete}
+      />
       <List
         rowComponent={ModeRunListItem}
         rowCount={modeRuns.length || 0}
         rowHeight={(index) => (index === modeRuns.length - 1 ? 120 : 86)}
-        rowProps={{ modeRuns, mode }}
+        rowProps={{ modeRuns, mode, onModeRunDelete: setModeRunToDelete }}
         overscanCount={5}
       />
     </motion.div>
--- src/components/pages/modes/list/ModeRunListItem.tsx
+++ src/components/pages/modes/list/ModeRunListItem.tsx
@@ -1,22 +1,25 @@
+import { Button } from '@/components/ui/button'
 import type { ModeRun } from '@/data/entities/moderun'
 import type ModeDefinition from '@/interfaces/data/mode/ModeDefinition'
 import NumberFlow from '@number-flow/react'
 import clsx from 'clsx'
 import dayjs from 'dayjs'
-import { Slash } from 'lucide-react'
+import { Slash, Trash2 } from 'lucide-react'
 import { useEffect, useMemo, useState } from 'react'
 import type { RowComponentProps } from 'react-window'
 
 interface Props {
   mode: ModeDefinition
   modeRuns: ModeRun[]
+  onModeRunDelete: (modeRun: ModeRun) => void
 }
 
 export default function ModeRunListItem({
   mode,
   modeRuns,
   index,
-  style
+  style,
+  onModeRunDelete
 }: RowComponentProps<Props>) {
   const modeRun = modeRuns[index]
 
@@ -51,20 +54,29 @@ export default function ModeRunListItem({
     <div style={style} className="">
       <div
         className={clsx(
-          'bg-card rounded-3xl border px-5 py-4',
+          'bg-card flex items-center justify-between rounded-3xl border px-5 py-4',
           dnf && 'opacity-50'
         )}
       >
-        <p className="mb-0 flex items-center gap-3 font-mono text-xs font-bold text-neutral-400">
-          # {modeRun.id} <Slash className="inline-block size-2" /> {date}
-        </p>
-        <p className="-mb-2 font-mono text-2xl font-bold">
-          {dnf ? (
-            <span className="block h-[36px]">DNF</span>
-          ) : (
-            <NumberFlow value={score} />
-          )}
-        </p>
+        <div>
+          <p className="mb-0 flex items-center gap-3 font-mono text-xs font-bold text-neutral-400">
+            # {modeRun.id} <Slash className="inline-block size-2" /> {date}
+          </p>
+          <p className="-mb-2 font-mono text-2xl font-bold">
+            {dnf ? (
+              <span className="block h-[36px]">DNF</span>
+            ) : (
+              <NumberFlow value={score} />
+            )}
+          </p>
+        </div>
+        <Button
+          variant="destructive"
+          size="icon"
+          onClick={() => onModeRunDelete(modeRun)}
+        >
+          <Trash2 />
+        </Button>
       </div>
     </div>
   )
