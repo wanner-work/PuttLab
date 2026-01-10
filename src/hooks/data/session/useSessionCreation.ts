08f7351d06d8bb171ca72cb2c3faf4d03363d2a0
Jonas Wanner <jonas.wanner@clavisit.com>
2026-01-10 22:08:39 +0100

    fix wrong function name

--- src/hooks/data/session/useSessionCreation.ts
+++ src/hooks/data/session/useSessionCreation.ts
@@ -2,7 +2,9 @@ import type { Session } from '@/data/entities/session'
 import createSession from '@/methods/data/create/createSession'
 import { useMutation } from '@tanstack/react-query'
 
-export default function useSessionMutation(onSuccess?: (session: Session) => void) {
+export default function useSessionCreation(
+  onSuccess?: (session: Session) => void
+) {
   return useMutation({
     mutationFn: createSession,
     onSuccess: (data) => {
